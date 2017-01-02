9979d1f1cba3297ae4fdad6fa0e8872084b36419
lmc2179 <louiscialdella@gmail.com>
2017-01-02 09:32:26 -0800

    Factored out data/regression object

--- regression_prototype.py
+++ regression_prototype.py
@@ -7,11 +7,22 @@ import seaborn as sns
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 from sklearn.linear_model import LinearRegression
+from collections import namedtuple
+
+class RegressionData(object):
+    def __init__(self, design_matrix):
+        self.design_matrix = design_matrix
+        self.target_variable_index = 1
+
+    def get_training_data(self):
+        return self.design_matrix[:, 1-self.target_variable_index], self.design_matrix[:,self.target_variable_index]
+
+    def flip(self):
+        self.target_variable_index = 1 - self.target_variable_index
 
 class RegressionGUIPrototype(object):
-    def __init__(self, d1, d2): #TODO: There is a lot of state here - can we encapsulate some of this?
-        self.d1 = d1
-        self.d2 = d2
+    def __init__(self, data_matrix): #TODO: There is a lot of state here - can we encapsulate some of this?
+        self.regression_data = RegressionData(data_matrix)
         self.figure = Figure(figsize=(5, 4), dpi=100)
         self.axes = self.figure.add_subplot(111)
         self.master = Tk()
@@ -20,12 +31,12 @@ class RegressionGUIPrototype(object):
         self.left_frame.pack(side=LEFT)
         self.right_frame.pack(side=RIGHT)
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.left_frame)
-        self.x, self.y = self.d1, self.d2
 
     def run(self):
         self._pack_flip_button(self.right_frame)
         self._pack_draw_fit_button(self.right_frame)
-        self._pack_plot(self.d1, self.d2, self.axes, self.canvas)
+        x, y = self.regression_data.get_training_data()
+        self._pack_plot(x, y, self.axes, self.canvas)
         self._pack_navigation(self.canvas, self.left_frame)
         self.master.mainloop()
 
@@ -57,9 +68,10 @@ class RegressionGUIPrototype(object):
     def _flip_axes_and_redraw(self):
         #TODO: Too much internal state access? Could make this pure and construct a callback via partial fxn
         #TODO: But this is hard because x/y are part of state
-        self.x, self.y = self.y, self.x
+        self.regression_data.flip()
+        x, y = self.regression_data.get_training_data()
         self._clear_axes(self.axes)
-        self._scatter_on_axes(self.x, self.y, self.axes)
+        self._scatter_on_axes(x, y, self.axes)
         self.canvas.draw()
 
     def _pack_draw_fit_button(self, master):
@@ -70,11 +82,12 @@ class RegressionGUIPrototype(object):
 
     def _toggle_draw_fit(self, checkbutton_var=None):
         self._clear_axes(self.axes)
-        self._scatter_on_axes(self.x, self.y, self.axes)
+        x, y = self.regression_data.get_training_data()
+        self._scatter_on_axes(x, y, self.axes)
         self.canvas.draw()
         if checkbutton_var.get():
-            m = LinearRegression().fit(self.x.reshape(-1, 1), self.y)
-            x_plot = np.linspace(min(self.x), max(self.x), num=100).reshape(-1, 1)
+            m = LinearRegression().fit(x.reshape(-1, 1), y)
+            x_plot = np.linspace(min(x), max(x), num=100).reshape(-1, 1)
             y_plot = m.predict(x_plot)
             self._plot_on_axes(x_plot, y_plot, self.axes)
             self.canvas.draw()
@@ -82,7 +95,8 @@ class RegressionGUIPrototype(object):
 if __name__ == '__main__':
     t = np.arange(0.0, 10.0, 0.01)
     s = t*0.5 + 10 + np.random.normal(0, 1, len(t))
-    RegressionGUIPrototype(t, s).run()
+    data_matrix = np.array([t, s]).T
+    RegressionGUIPrototype(data_matrix).run()
 
 #TODO: Convert this into the full 2D visual Regression prototype in your notebook
 #TODO: Add reading from CSV to complete prototype stage
